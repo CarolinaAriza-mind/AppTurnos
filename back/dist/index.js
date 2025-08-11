0653e77b2800e234472f22f9d0f54bdd7c005f9d
CarolinaAT <carolinaarizat93@gmail.com>
2025-08-11 15:59:30 -0300

    Pruebas con archivo de ts

---
+++ back/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var num1 = 24;
+var palabra = "Alegre";
+function saludar(num1, palabra) {
+    var saludo = "Estoy muy ".concat(palabra, ", cumplo ").concat(num1);
+    return saludo;
+}
+saludar(num1, palabra);
+var carlos = {
+    name: "Carlos",
+    age: 34,
+    active: true,
+    cargo: "jefe",
+    time: 20
+};
