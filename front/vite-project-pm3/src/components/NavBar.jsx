f15dbc83a0fa6446856edb18c68c01001980aa39
CarolinaAT <carolinaarizat93@gmail.com>
2025-09-07 20:59:45 -0300

    mejorando presentacion de la pagina y aplicando estilos con css modular

--- front/vite-project-pm3/src/components/NavBar.jsx
+++ front/vite-project-pm3/src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import style from "../styles/NavBar.module.css"
 const NavBar = () => {
   return (
     <div className= {style.containerNav}>
-      <ul className={style.item}><a>Home</a></ul> 
+      <ul className={style.item}><a>Inicio</a></ul> 
        <ul className={style.item}><a>Agenda tu cita</a></ul>
        <ul className={style.item}><a>Nuestras Sesiones</a> </ul>
-       <ul className={style.item}><a>Register</a> </ul>
-       <ul className={style.item}><a>Login</a></ul>
+       <ul className={style.item}><a>Registrate aqui</a> </ul>
+       <ul className={style.item}><a>Ingresa</a></ul>
     </div>
   );
 };
--- front/vite-project-pm3/src/views/Home.jsx
+++ front/vite-project-pm3/src/views/Home.jsx
@@ -1,13 +1,36 @@
 import NavBar from "../components/NavBar";
-import style from "../styles/Home.module.css"
+import style from "../styles/Home.module.css";
+import styles from "../styles/NavBar.module.css"
 
 const Home = () => {
   return (
-    <div className={style.containerHome}>
-      <NavBar />
-      <h1 className={style.title}>Bienvenido a RenaSer</h1>
-      <p className={style.subtitle}>un espacio para conectar con lo mas profundo de tu ser</p>
-      <img id={style.imgPresentacion} src="https://st.depositphotos.com/1001951/4735/i/450/depositphotos_47357993-stock-photo-fragility-of-a-human-creature.jpg"></img>
+    <div>
+      <div className={style.containerHome}>
+        <NavBar />
+        <h1 className={style.title}>Bienvenido a RenaSer</h1>
+        <p className={style.subtitle}>
+          un espacio para conectar con lo mas profundo de tu ser
+        </p>
+      </div>
+      <section id={style.seccionPresentacion}>
+        <div>
+          <img
+            id={style.imgPresentacion}
+            src="https://st.depositphotos.com/1001951/4735/i/450/depositphotos_47357993-stock-photo-fragility-of-a-human-creature.jpg"
+          ></img>
+        </div>
+        <div className={style.textoPresentacion}>
+          <h2 className={style.titles}>La mejor forma de predecir el futuro, es Creandolo</h2>
+          <p className={style.textParrafoP}>
+            Aprende a observar tu realidad, como un conjunto de herramientas
+            para poder vivir la vida que soñaste.
+            Nuestras terapias, te ofrecen una guia necesaria, para poder utilizar estas herramientas a tu favor! 
+          </p>
+          <div className={style.botones}>
+          <button className={styles.item}> Quiero tomar mi turno </button>
+        </div>
+        </div>
+      </section>
     </div>
   );
 };
