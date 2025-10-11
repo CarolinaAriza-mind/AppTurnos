cb5bc5fb0672ac76efa7ac7b6b2c04c68c51ef3a
CarolinaAT <carolinaarizat93@gmail.com>
2025-10-11 17:44:30 -0300

    Fix:Config Produccion

--- back/dist/DTO/AppointmentDTO.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Status = void 0;
-var Status;
-(function (Status) {
-    Status["active"] = "activo";
-    Status["cancel"] = "cancelado";
-})(Status || (exports.Status = Status = {}));
