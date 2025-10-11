e1ea2ceb66908567ba73c426ce33d2b8336997ca
CarolinaAT <carolinaarizat93@gmail.com>
2025-10-11 16:47:51 -0300

    Produccion

---
+++ back/dist/DTO/AppointmentDTO.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Status = void 0;
+var Status;
+(function (Status) {
+    Status["active"] = "activo";
+    Status["cancel"] = "cancelado";
+})(Status || (exports.Status = Status = {}));
